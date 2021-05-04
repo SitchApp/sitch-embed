0a9bc6c5442aa5414f6fd78407faa7302869a709
Nicholas Fisher <avidnick@gmail.com>
2021-05-04 12:29:10 -0400

    Using per session iframe caching.

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export default (
     // In this case Sitch has already been initialized and we just have to initialize any new buttons.
     _sitch_reinitializeButtons();
   } else {
+    const sessionId = Date.now();
     const globalScope: any = window;
     const initSitchWidget = () => {
       document.documentElement.style.setProperty('--_sitch_max-content-width', `100vw`);
@@ -245,7 +246,7 @@ export default (
             maxWidth = Number(newButton.dataset.sitchMaxWidth) || 0;
             setWidth();
             if (iframe && iframe.contentWindow && sitchLink) {
-              const newIframeUrl = `${sitchLink}/?e=true&ew=${maxWidth}&v=${Date.now()}`;
+              const newIframeUrl = `${sitchLink}/?e=true&ew=${maxWidth}&v=${sessionId}`;
               if (oldIframeUrl !== newIframeUrl) {
                 oldIframeUrl = newIframeUrl;
                 startLoading();
