64a138a2e2e1e1d29b1e0ae68f6be65251866350
Nicholas Fisher <avidnick@gmail.com>
2021-11-23 02:03:36 -0500

    Loader is no longer necessary.

--- src/index.ts
+++ src/index.ts
@@ -52,9 +52,6 @@ export default (options: Partial<SitchOptions> | undefined = undefined) => {
       sitchEmbedContainer.innerHTML = /*html*/ `
         <div id="_sitch_full-screen-dimmer"></div>
         <div id="_sitch_iframe-container">
-          <div id="_sitch_loading-spinner-container">
-            <div id="_sitch_loader">Loading...</div>
-          </div>
           <iframe id="_sitch_iframe" src="${baseUrl}/?e=true&bgc=${mergedOptions.backgroundColor.replace(/#/g, '%23')}&v=${sessionId}" allow="payment *"></iframe>
         </div>
       `;
@@ -104,62 +101,6 @@ export default (options: Partial<SitchOptions> | undefined = undefined) => {
             width: 100%;
             border: none;
           }
-          #_sitch_loading-spinner-container {
-            background-color: rgba(0, 0, 0, 0.5);
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            z-index: ${mergedOptions.baseZIndex + 1};
-            top: 0;
-            left: 0;
-            display: none;
-            justify-conent: center;
-            align-items: center;
-          }
-          #_sitch_embed._sitch_loading #_sitch_loading-spinner-container {
-            display: flex;
-          }
-          #_sitch_loader,
-          #_sitch_loader:after {
-            border-radius: 50%;
-            width: 10em;
-            height: 10em;
-          }
-          #_sitch_loader {
-            margin: 60px auto;
-            font-size: 10px;
-            position: relative;
-            text-indent: -9999em;
-            border-top: 1.1em solid rgba(255, 255, 255, 0.2);
-            border-right: 1.1em solid rgba(255, 255, 255, 0.2);
-            border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
-            border-left: 1.1em solid #fff;
-            -webkit-transform: translateZ(0);
-            -ms-transform: translateZ(0);
-            transform: translateZ(0);
-            -webkit-animation: _sitch_load8 1.1s infinite linear;
-            animation: _sitch_load8 1.1s infinite linear;
-          }
-          @-webkit-keyframes _sitch_load8 {
-            0% {
-              -webkit-transform: rotate(0deg);
-              transform: rotate(0deg);
-            }
-            100% {
-              -webkit-transform: rotate(360deg);
-              transform: rotate(360deg);
-            }
-          }
-          @keyframes _sitch_load8 {
-            0% {
-              -webkit-transform: rotate(0deg);
-              transform: rotate(0deg);
-            }
-            100% {
-              -webkit-transform: rotate(360deg);
-              transform: rotate(360deg);
-            }
-          }
           `;
       docHead.appendChild(styleSheet);
 
@@ -191,14 +132,6 @@ export default (options: Partial<SitchOptions> | undefined = undefined) => {
       };
 
       dimmer.onclick = hideSitch;
-
-      const showLoading = () => {
-        container.classList.add('_sitch_loading');
-      };
-
-      const hideLoading = () => {
-        container.classList.remove('_sitch_loading');
-      };
 
       let sitchLink: string;
       let maxWidth: number;
@@ -275,8 +208,6 @@ export default (options: Partial<SitchOptions> | undefined = undefined) => {
                 },
                 '*'
               );
-
-              showLoading(); // Show loading since we're updating the Sitch.
             } else {
               alert('This button does not have the required Sitch fields.');
             }
@@ -312,9 +243,6 @@ export default (options: Partial<SitchOptions> | undefined = undefined) => {
               break;
             case '_sitch_close':
               hideSitch();
-              break;
-            case '_sitch_loaded':
-              hideLoading();
               break;
             case '_sitch_mounted':
               _sitch_appMounted = true;
